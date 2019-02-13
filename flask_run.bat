9d58341d465cecf82ae208a902ff3aa4137ce259
Niksa <njadric@gmail.com>
2019-02-13 07:26:55 +0000

    First commit

---
+++ flask_run.bat
@@ -0,0 +1,4 @@
+@echo off
+set FLASK_APP=application.py
+set FLASK_DEBUG=1
+set DATABASE_URL=postgres://trhnlkctghtoer:469cb7a3c5be70fbbf2c88286fc75d61cb9b47534b5672ae17e7b1a3eb470310@ec2-79-125-4-96.eu-west-1.compute.amazonaws.com:5432/d291i9cv9pmpuu
